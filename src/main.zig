5f1814e00f4897bd4e9aaa15b6000cab5f518f57
xEgoist <egoist@egoistic.dev>
2022-07-19 22:57:32 -0500

    added miller rabin test and its test all functionalities should be implemented now. it should be possible to start making a simple RSA implementation

--- src/main.zig
+++ src/main.zig
@@ -4,6 +4,7 @@ const Managed = std.math.big.int.Managed;
 const Allocator = std.mem.Allocator;
 const test_allocator = std.testing.allocator;
 
+// 2048 -> ~4096
 const RSA_SIZE = 2048;
 
 const Inner = struct {
@@ -86,8 +87,35 @@ fn toggleRandomBits(r: *Managed, iterations: u16) !void {
     }
 }
 
+// Generates random number by toggling random bits of the provided r.
+// this function acts similarly to toggleRandomBits. However, it will assert that the number outputted is > start and < end.
+fn toggleRandomBitsRanged(r: *Managed, iterations: u16, start: Managed, end: Managed) !void {
+    var seed = std.time.milliTimestamp();
+    if (seed < 0) {
+        std.log.warn("Bad seed, using constant seed", .{});
+        seed = 42;
+    }
+    var rand = std.rand.DefaultPrng.init(@intCast(u64, seed));
+    var i: u16 = 0;
+    while (i <= iterations) : (i += 1) {
+        var num = rand.random().int(u16);
+        if (num > RSA_SIZE) {
+            num %= RSA_SIZE;
+        }
+        //std.log.warn("{}\n",.{num});
+        try toggle(r, num);
+    }
+    var temp = try Managed.initSet(r.allocator, 1);
+    defer temp.deinit();
+    try Managed.add(&temp, &start, &temp);
+    try Managed.sub(&temp, &end, &temp);
+    try Managed.divFloor(&temp, r, r, &temp);
+    try Managed.add(r, r, &start);
+}
+
 fn truncate(r: *Managed, bits: u16) !void {
     var one = try Managed.initSet(r.allocator, 1);
+    defer one.deinit();
     try one.shiftLeft(&one, bits);
     var one_again = try Managed.initSet(r.allocator, 1);
     defer one_again.deinit();
@@ -181,6 +209,95 @@ pub fn powMod(b: Managed, e: Managed, m: Managed) !Managed {
         try Managed.divFloor(&temp, &apow, &apow, &m);
     }
     return accum;
+}
+
+fn millerRabin(num: Managed, iters: *u16) !bool {
+    if (num.eqZero()) {
+        return false;
+    }
+    // used to compare the num, and see if it's <= 5.
+    var six = try Managed.initSet(num.allocator, 6);
+    defer six.deinit();
+    if (num.toConst().order(six.toConst()) == std.math.Order.lt) {
+        return true;
+    }
+    if (num.isEven()) {
+        return false;
+    }
+
+    //:outer random num constants
+    var lower = try Managed.initSet(num.allocator, 1);
+    defer lower.deinit();
+    var higher = try num.clone();
+    defer higher.deinit();
+    try Managed.sub(&higher, &higher, &lower);
+    // rand constants end
+    //
+    var two = try Managed.initSet(num.allocator, 2);
+    defer two.deinit();
+    var r: u64 = 0;
+    var s = try Managed.initSet(num.allocator, 1);
+    defer s.deinit();
+    try Managed.sub(&s, &num, &s);
+    while (s.isEven()) {
+        r += 1;
+        try s.shiftRight(&s, 1);
+    }
+    outer: while (iters.* > 0) : (iters.* -= 1) {
+        var a = try Managed.initSet(num.allocator, 0);
+        defer a.deinit();
+        try toggleRandomBitsRanged(&a, 100, lower, higher);
+        var x = try powMod(a, s, num);
+        defer x.deinit();
+
+        if (x.toConst().order(lower.toConst()) == std.math.Order.eq or
+            x.toConst().order(higher.toConst()) == std.math.Order.eq)
+        {
+            continue :outer;
+        }
+        var z: u64 = 1;
+        inner: while (z < r) : (z += 1) {
+            var temp00 = x;
+            defer temp00.deinit();
+            x = try powMod(temp00, two, num);
+            if (x.toConst().order(lower.toConst()) == std.math.Order.eq) {
+                return false;
+            } else if (x.toConst().order(higher.toConst()) == std.math.Order.eq) {
+                break :inner;
+            }
+        }
+        if (z == r) {
+            return false;
+        }
+    }
+    return true;
+}
+
+test "Miller Rabin Test Test" {
+    var prime = try Managed.initSet(test_allocator, 0);
+    try prime.setString(10, "190924658555315858151119591629547667189398663156457464802722656138791473781208916582860638604319810040699438425180594060124689945423307189481337028373");
+    defer prime.deinit();
+    var iters: u16 = 40;
+    var result = try millerRabin(prime, &iters);
+    try testing.expectEqual(result, true);
+}
+
+test "Miller Rabin Test Test 1" {
+    var prime = try Managed.initSet(test_allocator, 0);
+    try prime.setString(10, "23");
+    defer prime.deinit();
+    var iters: u16 = 40;
+    var result = try millerRabin(prime, &iters);
+    try testing.expectEqual(result, true);
+}
+
+test "Miller Rabin Test Test 2" {
+    var prime = try Managed.initSet(test_allocator, 0);
+    try prime.setString(10, "420");
+    defer prime.deinit();
+    var iters: u16 = 40;
+    var result = try millerRabin(prime, &iters);
+    try testing.expectEqual(result, false);
 }
 
 test "Pow Mod" {
