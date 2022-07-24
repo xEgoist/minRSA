348f01e4e7220507b90b6a04ec1448707c55ce75
xEgoist <egoist@egoistic.dev>
2022-07-23 20:22:48 -0500

    maybe this is better

--- src/main.zig
+++ src/main.zig
@@ -149,7 +149,7 @@ fn truncate(r: *Managed, bits: u16) !void {
 fn generateDevRandom(alloc: Allocator) !Managed {
     if (builtin.os.tag == .windows) {
         var hCryptProv: w.HCRYPTPROV = undefined;
-        var context: w.BOOL = CryptAcquireContext(&hCryptProv, null, null, w.PROV_RSA_FULL, w.CRYPT_VERIFYCONTEXT);
+        var context: w.BOOL = CryptAcquireContext(&hCryptProv, "MYKeyContainer", null, w.PROV_RSA_FULL, w.CRYPT_VERIFYCONTEXT);
         if (context) {
         var pbData: [RSA_SIZE]w.BYTE = [_]w.BYTE{0} ** RSA_SIZE;
         const ptr = @ptrCast(*w.BYTE, &pbData);
