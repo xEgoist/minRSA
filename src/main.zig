5c201f68ba1c91bae8a6d0924c03b37901fcfec4
xEgoist <egoist@egoistic.dev>
2022-07-26 00:48:14 -0500

    Added documentation for functions + made useful functions public

--- src/main.zig
+++ src/main.zig
@@ -9,6 +9,8 @@ const w = std.os.windows;
 // 256 bytes -> 2048 -> ~4096
 const RSA_SIZE = 128;
 
+// Inner struct of RSA. This is used to seperate the allocator from the internal RSA parameters.
+// This can be later utilized for creating PKCS1
 const Inner = struct {
     p: Managed,
     q: Managed,
@@ -19,6 +21,7 @@ const Inner = struct {
 };
 
 //Windows Only
+// Using the bcrypt header to generate cryptographically secure random numbers.
 pub extern "bcrypt" fn BCryptGenRandom(
     //*void / BCRYPT_ALG_HANDLE
     hAlgorithm: ?*anyopaque,
@@ -34,7 +37,7 @@ pub const RSA = struct {
     allocator: Allocator,
     inner: *Inner,
 
-    fn init(alloc: Allocator) !RSA {
+    pub fn init(alloc: Allocator) !RSA {
         var inner = try alloc.create(Inner);
         inner.* = Inner{
             .p = try generatePrimeThreaded(alloc),
@@ -60,7 +63,7 @@ pub const RSA = struct {
         };
     }
 
-    fn deinit(self: *RSA) void {
+    pub fn deinit(self: *RSA) void {
         self.inner.phi.deinit();
         self.inner.p.deinit();
         self.inner.q.deinit();
@@ -216,8 +219,8 @@ fn generateDevRandom(alloc: Allocator, fd: *?std.fs.File) !Managed {
 // Makes an arbitrary number out of a string.
 // Takes the string, converts it to a lower hex slice without 0x, then uses Managed to set it from
 // a "string".
-fn numbify(string: []const u8, alloc: Allocator) !Managed {
-    var ret = try Managed.initSet(alloc, 1);
+pub fn numbify(string: []const u8, alloc: Allocator) !Managed {
+    var ret = try Managed.init(alloc);
     const hexed = try std.fmt.allocPrint(
         alloc,
         "{x}",
@@ -228,7 +231,9 @@ fn numbify(string: []const u8, alloc: Allocator) !Managed {
     return ret;
 }
 
-fn denumbify(input: []const u8, alloc: Allocator) ![]u8 {
+// Oposite of numbify. Takes in a string of a big int, converts it back to hex
+// then converts that hex to bytes to construct the original string.
+pub fn denumbify(input: []const u8, alloc: Allocator) ![]u8 {
     var ret = try Managed.initSet(alloc, 1);
     try ret.setString(10, input);
     defer ret.deinit();
@@ -239,8 +244,9 @@ fn denumbify(input: []const u8, alloc: Allocator) ![]u8 {
     var decoded = try std.fmt.hexToBytes(val, tt);
     return decoded;
 }
-
-fn modinv(a0: Managed, m0: Managed) !Managed {
+// Calculates Modular Multiplicative Inverse.
+// This function will use the same allocator as a0 for internal allocations
+pub fn modinv(a0: Managed, m0: Managed) !Managed {
     var one = try Managed.initSet(a0.allocator, 1);
     if (m0.toConst().order(one.toConst()) == std.math.Order.eq) {
         return one;
@@ -303,6 +309,9 @@ pub fn powMod(b: Managed, e: Managed, m: Managed) !Managed {
     return accum;
 }
 
+// Miller Rabin primality test where iterations is the number of rounds.
+// true => number is probably prime
+// false => number is definately not a prime
 fn millerRabin(num: Managed, iterations: u16) !bool {
     var iters = iterations;
     if (num.eqZero()) {
@@ -366,8 +375,13 @@ fn millerRabin(num: Managed, iterations: u16) !bool {
     return true;
 }
 
+// Primes less than 5000. This is to help speed up the process for finding odd numbers that aren't primes.
 const LowerPrimes = [_]u32{ 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997, 1009, 1013, 1019, 1021, 1031, 1033, 1039, 1049, 1051, 1061, 1063, 1069, 1087, 1091, 1093, 1097, 1103, 1109, 1117, 1123, 1129, 1151, 1153, 1163, 1171, 1181, 1187, 1193, 1201, 1213, 1217, 1223, 1229, 1231, 1237, 1249, 1259, 1277, 1279, 1283, 1289, 1291, 1297, 1301, 1303, 1307, 1319, 1321, 1327, 1361, 1367, 1373, 1381, 1399, 1409, 1423, 1427, 1429, 1433, 1439, 1447, 1451, 1453, 1459, 1471, 1481, 1483, 1487, 1489, 1493, 1499, 1511, 1523, 1531, 1543, 1549, 1553, 1559, 1567, 1571, 1579, 1583, 1597, 1601, 1607, 1609, 1613, 1619, 1621, 1627, 1637, 1657, 1663, 1667, 1669, 1693, 1697, 1699, 1709, 1721, 1723, 1733, 1741, 1747, 1753, 1759, 1777, 1783, 1787, 1789, 1801, 1811, 1823, 1831, 1847, 1861, 1867, 1871, 1873, 1877, 1879, 1889, 1901, 1907, 1913, 1931, 1933, 1949, 1951, 1973, 1979, 1987, 1993, 1997, 1999, 2003, 2011, 2017, 2027, 2029, 2039, 2053, 2063, 2069, 2081, 2083, 2087, 2089, 2099, 2111, 2113, 2129, 2131, 2137, 2141, 2143, 2153, 2161, 2179, 2203, 2207, 2213, 2221, 2237, 2239, 2243, 2251, 2267, 2269, 2273, 2281, 2287, 2293, 2297, 2309, 2311, 2333, 2339, 2341, 2347, 2351, 2357, 2371, 2377, 2381, 2383, 2389, 2393, 2399, 2411, 2417, 2423, 2437, 2441, 2447, 2459, 2467, 2473, 2477, 2503, 2521, 2531, 2539, 2543, 2549, 2551, 2557, 2579, 2591, 2593, 2609, 2617, 2621, 2633, 2647, 2657, 2659, 2663, 2671, 2677, 2683, 2687, 2689, 2693, 2699, 2707, 2711, 2713, 2719, 2729, 2731, 2741, 2749, 2753, 2767, 2777, 2789, 2791, 2797, 2801, 2803, 2819, 2833, 2837, 2843, 2851, 2857, 2861, 2879, 2887, 2897, 2903, 2909, 2917, 2927, 2939, 2953, 2957, 2963, 2969, 2971, 2999, 3001, 3011, 3019, 3023, 3037, 3041, 3049, 3061, 3067, 3079, 3083, 3089, 3109, 3119, 3121, 3137, 3163, 3167, 3169, 3181, 3187, 3191, 3203, 3209, 3217, 3221, 3229, 3251, 3253, 3257, 3259, 3271, 3299, 3301, 3307, 3313, 3319, 3323, 3329, 3331, 3343, 3347, 3359, 3361, 3371, 3373, 3389, 3391, 3407, 3413, 3433, 3449, 3457, 3461, 3463, 3467, 3469, 3491, 3499, 3511, 3517, 3527, 3529, 3533, 3539, 3541, 3547, 3557, 3559, 3571, 3581, 3583, 3593, 3607, 3613, 3617, 3623, 3631, 3637, 3643, 3659, 3671, 3673, 3677, 3691, 3697, 3701, 3709, 3719, 3727, 3733, 3739, 3761, 3767, 3769, 3779, 3793, 3797, 3803, 3821, 3823, 3833, 3847, 3851, 3853, 3863, 3877, 3881, 3889, 3907, 3911, 3917, 3919, 3923, 3929, 3931, 3943, 3947, 3967, 3989, 4001, 4003, 4007, 4013, 4019, 4021, 4027, 4049, 4051, 4057, 4073, 4079, 4091, 4093, 4099, 4111, 4127, 4129, 4133, 4139, 4153, 4157, 4159, 4177, 4201, 4211, 4217, 4219, 4229, 4231, 4241, 4243, 4253, 4259, 4261, 4271, 4273, 4283, 4289, 4297, 4327, 4337, 4339, 4349, 4357, 4363, 4373, 4391, 4397, 4409, 4421, 4423, 4441, 4447, 4451, 4457, 4463, 4481, 4483, 4493, 4507, 4513, 4517, 4519, 4523, 4547, 4549, 4561, 4567, 4583, 4591, 4597, 4603, 4621, 4637, 4639, 4643, 4649, 4651, 4657, 4663, 4673, 4679, 4691, 4703, 4721, 4723, 4729, 4733, 4751, 4759, 4783, 4787, 4789, 4793, 4799, 4801, 4813, 4817, 4831, 4861, 4871, 4877, 4889, 4903, 4909, 4919, 4931, 4933, 4937, 4943, 4951, 4957, 4967, 4969, 4973, 4987, 4993, 4999 };
 
+// Miller Rabin primality test. This functions returns a void instead of a bool.
+// However, it takes in the bool as a supplied argument. This is helpful for the usage of threads.
+// ret.* = true => Probably Prime
+// ret.* = false => definately not a prime
 fn millerRabinThreadHelped(ret: *bool, num: Managed, iterations: u16) !void {
     var iters = iterations;
     if (num.eqZero()) {
@@ -397,7 +411,6 @@ fn millerRabinThreadHelped(ret: *bool, num: Managed, iterations: u16) !void {
     defer higher.deinit();
     try Managed.sub(&higher, &higher, &lower);
     // rand constants end
-    //
     var two = try Managed.initSet(num.allocator, 2);
     defer two.deinit();
     var r: u64 = 0;
@@ -409,6 +422,11 @@ fn millerRabinThreadHelped(ret: *bool, num: Managed, iterations: u16) !void {
         try s.shiftRight(&s, 1);
     }
     var file = std.fs.cwd().openFile("/dev/urandom", .{}) catch null;
+    defer {
+        if (file != null) {
+            file.?.close();
+        }
+    }
     outer: while (iters > 0) : (iters -= 1) {
         var a = try generateDevRandomRanged(num.allocator, &file, lower, higher);
         defer a.deinit();
@@ -426,9 +444,6 @@ fn millerRabinThreadHelped(ret: *bool, num: Managed, iterations: u16) !void {
             defer temp00.deinit();
             x = try powMod(temp00, two, num);
             if (x.toConst().order(lower.toConst()) == std.math.Order.eq) {
-                if (file != null) {
-                    file.?.close();
-                }
                 ret.* = false;
                 return;
             } else if (x.toConst().order(higher.toConst()) == std.math.Order.eq) {
@@ -436,25 +451,20 @@ fn millerRabinThreadHelped(ret: *bool, num: Managed, iterations: u16) !void {
             }
         }
         if (z == r) {
-            if (file != null) {
-                file.?.close();
-            }
             ret.* = false;
             return;
         }
-    }
-    if (file != null) {
-        file.?.close();
     }
     ret.* = true;
     return;
 }
 
+// single threaded prime generation function. takes in the allocator for the prime generated.
+// It will continueously run generateDevRandom until the result of millerRabin is true.
 fn generate_prime(alloc: Allocator) !Managed {
     var candy = try generateDevRandom(alloc, null);
     var exit = try millerRabin(candy, 40);
     while (exit != true) {
-        std.debug.print("\nCandy {}\n", .{candy});
         candy.deinit();
         candy = try generateDevRandom(alloc, null);
         // toggle 1 more random bit and see it that makes it a prime
@@ -467,7 +477,7 @@ fn generate_prime(alloc: Allocator) !Managed {
 // Similar to generate_prime. However, this function is threaded for optimization.
 // takes in the allocator which will be used to allocate the prime candidate and the thread pool.
 const ThreadCount: usize = 100;
-fn generatePrimeThreaded(alloc: Allocator) !Managed {
+pub fn generatePrimeThreaded(alloc: Allocator) !Managed {
     var ret: Managed = undefined;
     var exit = true;
     var file = std.fs.cwd().openFile("/dev/urandom", .{}) catch null;
